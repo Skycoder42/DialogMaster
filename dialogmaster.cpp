9f518c61651d3f8454e54f1111dd9c0783dd9bc4
Felix Barz <Skycoder42@users.noreply.github.com>
2016-03-31 21:48:51 +0200

    added the namespace

---
+++ dialogmaster.cpp
@@ -0,0 +1,395 @@
+#include "dialogmaster.h"
+#include <QCoreApplication>
+#include <QProgressDialog>
+#include <QProgressBar>
+
+class CloseFilter : public QObject
+{
+public:
+	inline CloseFilter(QObject *parent) :
+		QObject(parent)
+	{}
+	inline bool eventFilter(QObject *, QEvent *ev) Q_DECL_OVERRIDE {
+		if(ev->type() == QEvent::Close) {
+			ev->ignore();
+			return true;
+		} else
+			return false;
+	}
+};
+
+bool DialogMaster::TitleAsWindowTitle = false;
+
+void DialogMaster::masterDialog(QDialog *dialog, bool fixedSize, Qt::WindowFlags additionalFlags)
+{
+	Qt::WindowFlags flags = Qt::WindowTitleHint |
+							Qt::WindowSystemMenuHint |
+							Qt::WindowCloseButtonHint |
+							(fixedSize ? Qt::MSWindowsFixedSizeDialogHint : Qt::Window) |
+							additionalFlags;
+
+	dialog->setSizeGripEnabled(!fixedSize);
+	if(dialog->parentWidget()) {
+		dialog->setWindowModality(Qt::WindowModal);
+		flags |= Qt::Sheet;
+	} else {
+		dialog->setWindowModality(Qt::ApplicationModal);
+		flags |= Qt::Dialog;
+	}
+	dialog->setWindowFlags(flags);
+}
+
+QMessageBox::StandardButton DialogMaster::msgBox(QWidget *parent, DialogMaster::MessageBoxIcon icon, const QString &text, const QString &title, const QString &windowTitle, const QString &details, bool *checked, const QString &checkString, QMessageBox::StandardButtons buttons, QMessageBox::StandardButton defaultButton, QMessageBox::StandardButton escapeButton)
+{
+	QMessageBox msgBox(parent);
+	if(icon.isCustom)
+		msgBox.setIconPixmap(icon.custIcon);
+	else
+		msgBox.setIcon(icon.mbxIcon);
+	msgBox.setWindowTitle(DialogMaster::TitleAsWindowTitle ? title : windowTitle);
+	if(DialogMaster::TitleAsWindowTitle || title.isEmpty())
+		msgBox.setText(text);
+	else {
+		msgBox.setText(QStringLiteral("<b>%1</b>").arg(title));
+		msgBox.setInformativeText(text);
+	}
+	if(checked) {
+		QCheckBox *box = new QCheckBox(checkString, &msgBox);
+		box->setChecked(*checked);
+		QObject::connect(box, &QCheckBox::toggled, &msgBox, [checked](bool isChecked){
+			*checked = isChecked;
+		});
+		msgBox.setCheckBox(box);
+	}
+	msgBox.setStandardButtons(buttons);
+	msgBox.setDefaultButton(defaultButton);
+	msgBox.setEscapeButton(escapeButton);
+	msgBox.setDetailedText(details);
+
+#ifdef Q_OS_WINCE
+	Qt::WindowFlags flags = 0;
+	if(buttons.testFlag(QMessageBox::Ok))
+		flags |= Qt::WindowOkButtonHint;
+	if(buttons.testFlag(QMessageBox::Cancel))
+		flags |= Qt::WindowCancelButtonHint;
+	DialogMaster::masterDialog(&msgBox, true, flags);
+#else
+	DialogMaster::masterDialog(&msgBox, true);
+#endif
+
+	return (QMessageBox::StandardButton)msgBox.exec();
+}
+
+QMessageBox::StandardButton DialogMaster::information(QWidget *parent, const QString &text, const QString &title, const QString &windowTitle, QMessageBox::StandardButtons buttons, QMessageBox::StandardButton defaultButton, QMessageBox::StandardButton escapeButton)
+{
+	return msgBox(parent,
+				  QMessageBox::Information,
+				  text,
+				  title,
+				  windowTitle.isEmpty() ? QCoreApplication::translate("DialogMaster", "Information") : windowTitle,
+				  QString(),
+				  NULL,
+				  QString(),
+				  buttons,
+				  defaultButton,
+				  escapeButton);
+}
+
+QMessageBox::StandardButton DialogMaster::information(QWidget *parent, const QString &text, const QString &title, const QString &windowTitle, QMessageBox::StandardButton button1, QMessageBox::StandardButton button2)
+{
+	return msgBox(parent,
+				  QMessageBox::Information,
+				  text,
+				  title,
+				  windowTitle.isEmpty() ? QCoreApplication::translate("DialogMaster", "Information") : windowTitle,
+				  QString(),
+				  NULL,
+				  QString(),
+				  button1 | button2,
+				  button1,
+				  button2);
+}
+
+QMessageBox::StandardButton DialogMaster::question(QWidget *parent, const QString &text, const QString &title, const QString &windowTitle, QMessageBox::StandardButtons buttons, QMessageBox::StandardButton defaultButton, QMessageBox::StandardButton escapeButton)
+{
+	return msgBox(parent,
+				  QMessageBox::Question,
+				  text,
+				  title,
+				  windowTitle.isEmpty() ? QCoreApplication::translate("DialogMaster", "Question") : windowTitle,
+				  QString(),
+				  NULL,
+				  QString(),
+				  buttons,
+				  defaultButton,
+				  escapeButton);
+}
+
+QMessageBox::StandardButton DialogMaster::question(QWidget *parent, const QString &text, const QString &title, const QString &windowTitle, QMessageBox::StandardButton button1, QMessageBox::StandardButton button2)
+{
+	return msgBox(parent,
+				  QMessageBox::Question,
+				  text,
+				  title,
+				  windowTitle.isEmpty() ? QCoreApplication::translate("DialogMaster", "Question") : windowTitle,
+				  QString(),
+				  NULL,
+				  QString(),
+				  button1 | button2,
+				  button1,
+				  button2);
+}
+
+QMessageBox::StandardButton DialogMaster::warning(QWidget *parent, const QString &text, const QString &title, const QString &windowTitle, QMessageBox::StandardButtons buttons, QMessageBox::StandardButton defaultButton, QMessageBox::StandardButton escapeButton)
+{
+	return msgBox(parent,
+				  QMessageBox::Warning,
+				  text,
+				  title,
+				  windowTitle.isEmpty() ? QCoreApplication::translate("DialogMaster", "Warning") : windowTitle,
+				  QString(),
+				  NULL,
+				  QString(),
+				  buttons,
+				  defaultButton,
+				  escapeButton);
+}
+
+QMessageBox::StandardButton DialogMaster::warning(QWidget *parent, const QString &text, const QString &title, const QString &windowTitle, QMessageBox::StandardButton button1, QMessageBox::StandardButton button2)
+{
+	return msgBox(parent,
+				  QMessageBox::Warning,
+				  text,
+				  title,
+				  windowTitle.isEmpty() ? QCoreApplication::translate("DialogMaster", "Warning") : windowTitle,
+				  QString(),
+				  NULL,
+				  QString(),
+				  button1 | button2,
+				  button1,
+				  button2);
+}
+
+QMessageBox::StandardButton DialogMaster::critical(QWidget *parent, const QString &text, const QString &title, const QString &windowTitle, QMessageBox::StandardButtons buttons, QMessageBox::StandardButton defaultButton, QMessageBox::StandardButton escapeButton)
+{
+	return msgBox(parent,
+				  QMessageBox::Critical,
+				  text,
+				  title,
+				  windowTitle.isEmpty() ? QCoreApplication::translate("DialogMaster", "Error") : windowTitle,
+				  QString(),
+				  NULL,
+				  QString(),
+				  buttons,
+				  defaultButton,
+				  escapeButton);
+}
+
+QMessageBox::StandardButton DialogMaster::critical(QWidget *parent, const QString &text, const QString &title, const QString &windowTitle, QMessageBox::StandardButton button1, QMessageBox::StandardButton button2)
+{
+	return msgBox(parent,
+				  QMessageBox::Critical,
+				  text,
+				  title,
+				  windowTitle.isEmpty() ? QCoreApplication::translate("DialogMaster", "Error") : windowTitle,
+				  QString(),
+				  NULL,
+				  QString(),
+				  button1 | button2,
+				  button1,
+				  button2);
+}
+
+
+
+DialogMaster::MessageBoxIcon::MessageBoxIcon(QMessageBox::Icon mbxIcon) :
+	isCustom(false),
+	mbxIcon(mbxIcon),
+	custIcon()
+{}
+
+DialogMaster::MessageBoxIcon::MessageBoxIcon(const QPixmap &custIcon) :
+	isCustom(true),
+	mbxIcon(),
+	custIcon(custIcon.scaled(64, 64, Qt::KeepAspectRatio, Qt::SmoothTransformation))
+{}
+
+DialogMaster::MessageBoxIcon::MessageBoxIcon(const QIcon &custIcon) :
+	isCustom(true),
+	mbxIcon(),
+	custIcon(custIcon.pixmap(64, 64))
+{}
+
+double DialogMaster::getDouble(QWidget *parent, const QString &label, double value, double min, double max, const QString &title, int decimals, bool *ok)
+{
+	QInputDialog dialog(parent);
+	dialog.setWindowTitle(title);
+	dialog.setLabelText(label);
+	dialog.setInputMode(QInputDialog::DoubleInput);
+	dialog.setDoubleRange(min, max);
+	dialog.setDoubleDecimals(decimals);
+	dialog.setDoubleValue(value);
+
+	DialogMaster::masterDialog(&dialog, true);
+	if(dialog.exec() == QDialog::Accepted) {
+		if(ok)
+			*ok = true;
+		return dialog.doubleValue();
+	} else {
+		if(ok)
+			*ok = false;
+		return value;
+	}
+}
+
+int DialogMaster::getInt(QWidget *parent, const QString &label, int value, int min, int max, const QString &title, int step, bool *ok)
+{
+	QInputDialog dialog(parent);
+	dialog.setWindowTitle(title);
+	dialog.setLabelText(label);
+	dialog.setInputMode(QInputDialog::IntInput);
+	dialog.setIntRange(min, max);
+	dialog.setIntStep(step);
+	dialog.setIntValue(value);
+
+	DialogMaster::masterDialog(&dialog, true);
+	if(dialog.exec() == QDialog::Accepted) {
+		if(ok)
+			*ok = true;
+		return dialog.intValue();
+	} else {
+		if(ok)
+			*ok = false;
+		return value;
+	}
+}
+
+QString DialogMaster::getItem(QWidget *parent, const QString &label, const QStringList &items, const QString &title, int current, bool editable, bool *ok, Qt::InputMethodHints inputMethodHints)
+{
+	QInputDialog dialog(parent);
+	dialog.setWindowTitle(title);
+	dialog.setLabelText(label);
+	dialog.setInputMode(QInputDialog::TextInput);
+	dialog.setComboBoxItems(items);
+	dialog.setComboBoxEditable(editable);
+	dialog.setTextValue(items[current]);
+	dialog.setInputMethodHints(inputMethodHints);
+	dialog.setOptions(QInputDialog::UseListViewForComboBoxItems);
+
+	DialogMaster::masterDialog(&dialog);
+	if(dialog.exec() == QDialog::Accepted) {
+		if(ok)
+			*ok = true;
+		return dialog.textValue();
+	} else {
+		if(ok)
+			*ok = false;
+		return items[current];
+	}
+}
+
+int DialogMaster::getItemIndex(QWidget *parent, const QString &label, const QStringList &items, const QString &title, int current, bool *ok)
+{
+	QInputDialog dialog(parent);
+	dialog.setWindowTitle(title);
+	dialog.setLabelText(label);
+	dialog.setInputMode(QInputDialog::TextInput);
+	dialog.setComboBoxItems(items);
+	dialog.setComboBoxEditable(false);
+	dialog.setTextValue(items[current]);
+
+	DialogMaster::masterDialog(&dialog, true);
+	if(dialog.exec() == QDialog::Accepted) {
+		if(ok)
+			*ok = true;
+		return items.indexOf(dialog.textValue());
+	} else {
+		if(ok)
+			*ok = false;
+		return current;
+	}
+}
+
+QString DialogMaster::getMultiLineText(QWidget *parent, const QString &label, const QString &title, const QString &text, bool *ok, Qt::InputMethodHints inputMethodHints)
+{
+	QInputDialog dialog(parent);
+	dialog.setWindowTitle(title);
+	dialog.setLabelText(label);
+	dialog.setInputMode(QInputDialog::TextInput);
+	dialog.setTextValue(text);
+	dialog.setInputMethodHints(inputMethodHints);
+	dialog.setOption(QInputDialog::UsePlainTextEditForTextInput);
+
+	DialogMaster::masterDialog(&dialog);
+	if(dialog.exec() == QDialog::Accepted) {
+		if(ok)
+			*ok = true;
+		return dialog.textValue();
+	} else {
+		if(ok)
+			*ok = false;
+		return text;
+	}
+}
+
+QString DialogMaster::getText(QWidget *parent, const QString &label, const QString &title, QLineEdit::EchoMode mode, const QString &text, bool *ok, Qt::InputMethodHints inputMethodHints)
+{
+	QInputDialog dialog(parent);
+	dialog.setWindowTitle(title);
+	dialog.setLabelText(label);
+	dialog.setInputMode(QInputDialog::TextInput);
+	dialog.setTextEchoMode(mode);
+	dialog.setTextValue(text);
+	dialog.setInputMethodHints(inputMethodHints);
+
+	DialogMaster::masterDialog(&dialog);
+	if(dialog.exec() == QDialog::Accepted) {
+		if(ok)
+			*ok = true;
+		return dialog.textValue();
+	} else {
+		if(ok)
+			*ok = false;
+		return text;
+	}
+}
+
+QProgressDialog *DialogMaster::createProgress(QWidget *parent, const QString &label, const int max, const int min, bool allowCancel, const QString &windowTitle, int minimumDuration, const QString &cancelButtonText)
+{
+	QProgressDialog *dialog = new QProgressDialog(parent);
+	Qt::WindowFlags flags = Qt::CustomizeWindowHint |
+							Qt::MSWindowsFixedSizeDialogHint |
+							Qt::WindowTitleHint;
+
+	if(parent) {
+		dialog->setWindowModality(Qt::WindowModal);
+		flags |= Qt::Sheet;
+	} else {
+		dialog->setWindowModality(Qt::ApplicationModal);
+		flags |= Qt::Dialog;
+	}
+
+	dialog->setWindowFlags(flags);
+	dialog->setWindowTitle(windowTitle.isEmpty() ?
+							   (parent ? parent->windowTitle() : QString()) :
+							   windowTitle);
+	dialog->setSizeGripEnabled(false);
+
+	if(max == min && max == 0) {
+		QProgressBar *bar = new QProgressBar(dialog);
+		bar->setTextVisible(false);
+		dialog->setBar(bar);
+	}
+
+	dialog->setLabelText(label);
+	if(!allowCancel) {
+		dialog->installEventFilter(new CloseFilter(dialog));
+		dialog->setCancelButtonText(QString());
+	} else if(!cancelButtonText.isEmpty())
+		dialog->setCancelButtonText(cancelButtonText);
+	dialog->setMinimumDuration(minimumDuration);
+	dialog->setRange(min, max);
+	dialog->setValue(min);
+
+	return dialog;
+}
---
+++ dialogmaster.h
@@ -0,0 +1,164 @@
+#ifndef MESSAGEMASTER_H
+#define MESSAGEMASTER_H
+
+#include <QDialog>
+#include <QMessageBox>
+#include <QPixmap>
+#include <QCheckBox>
+#include <QInputDialog>
+class QProgressDialog;
+
+namespace DialogMaster
+{
+	// ---------- QDialog ----------
+	void masterDialog(QDialog *dialog, bool fixedSize = false, Qt::WindowFlags additionalFlags = 0);
+
+	// ---------- QMessageBox ----------
+	extern bool TitleAsWindowTitle;
+
+	struct MessageBoxIcon {
+		bool isCustom;
+		QMessageBox::Icon mbxIcon;
+		QPixmap custIcon;
+
+		MessageBoxIcon(QMessageBox::Icon mbxIcon);
+		MessageBoxIcon(const QPixmap &custIcon);
+		MessageBoxIcon(const QIcon &custIcon);
+	};
+
+	QMessageBox::StandardButton msgBox(QWidget *parent,
+									   MessageBoxIcon icon,
+									   const QString &text,
+									   const QString &title = QString(),
+									   const QString &windowTitle = QString(),
+									   const QString &details = QString(),
+									   bool *checked = NULL,
+									   const QString &checkString = QString(),
+									   QMessageBox::StandardButtons buttons = QMessageBox::Ok,
+									   QMessageBox::StandardButton defaultButton = QMessageBox::Ok,
+									   QMessageBox::StandardButton escapeButton = QMessageBox::Cancel);
+
+
+
+	QMessageBox::StandardButton information(QWidget *parent,
+											const QString &text,
+											const QString &title = QString(),
+											const QString &windowTitle = QString(),
+											QMessageBox::StandardButtons buttons = QMessageBox::Ok,
+											QMessageBox::StandardButton defaultButton = QMessageBox::Ok,
+											QMessageBox::StandardButton escapeButton = QMessageBox::Cancel);
+
+	QMessageBox::StandardButton information(QWidget *parent,
+											const QString &text,
+											const QString &title,
+											const QString &windowTitle,
+											QMessageBox::StandardButton button1,
+											QMessageBox::StandardButton button2);
+
+	QMessageBox::StandardButton question(QWidget *parent,
+										 const QString &text,
+										 const QString &title = QString(),
+										 const QString &windowTitle = QString(),
+										 QMessageBox::StandardButtons buttons = QMessageBox::StandardButtons(QMessageBox::Yes | QMessageBox::No),
+										 QMessageBox::StandardButton defaultButton = QMessageBox::Yes,
+										 QMessageBox::StandardButton escapeButton = QMessageBox::No);
+
+	QMessageBox::StandardButton question(QWidget *parent,
+										 const QString &text,
+										 const QString &title,
+										 const QString &windowTitle,
+										 QMessageBox::StandardButton button1,
+										 QMessageBox::StandardButton button2);
+
+	QMessageBox::StandardButton warning(QWidget *parent,
+										const QString &text,
+										const QString &title = QString(),
+										const QString &windowTitle = QString(),
+										QMessageBox::StandardButtons buttons = QMessageBox::Ok,
+										QMessageBox::StandardButton defaultButton = QMessageBox::Ok,
+										QMessageBox::StandardButton escapeButton = QMessageBox::Cancel);
+
+	QMessageBox::StandardButton warning(QWidget *parent,
+										const QString &text,
+										const QString &title,
+										const QString &windowTitle,
+										QMessageBox::StandardButton button1,
+										QMessageBox::StandardButton button2);
+
+	QMessageBox::StandardButton critical(QWidget *parent,
+										 const QString &text,
+										 const QString &title = QString(),
+										 const QString &windowTitle = QString(),
+										 QMessageBox::StandardButtons buttons = QMessageBox::Ok,
+										 QMessageBox::StandardButton defaultButton = QMessageBox::Ok,
+										 QMessageBox::StandardButton escapeButton = QMessageBox::Cancel);
+
+	QMessageBox::StandardButton critical(QWidget *parent,
+										 const QString &text,
+										 const QString &title,
+										 const QString &windowTitle,
+										 QMessageBox::StandardButton button1,
+										 QMessageBox::StandardButton button2);
+
+	// ---------- QInputDialog ----------
+	double getDouble(QWidget *parent,
+					 const QString &label,
+					 double value = 0,
+					 double min = INT_MIN,
+					 double max = INT_MAX,
+					 const QString &title = QString(),
+					 int decimals = 1,
+					 bool *ok = Q_NULLPTR);
+
+	int getInt(QWidget *parent,
+			   const QString &label,
+			   int value = 0,
+			   int min = INT_MIN,
+			   int max = INT_MAX,
+			   const QString &title = QString(),
+			   int step = 1,
+			   bool *ok = 0);
+
+	QString getItem(QWidget *parent,
+					const QString &label,
+					const QStringList &items,
+					const QString &title = QString(),
+					int current = 0,
+					bool editable = true,
+					bool *ok = Q_NULLPTR,
+					Qt::InputMethodHints inputMethodHints = Qt::ImhNone);
+
+	int getItemIndex(QWidget *parent,
+					 const QString &label,
+					 const QStringList &items,
+					 const QString &title = QString(),
+					 int current = 0,
+					 bool *ok = Q_NULLPTR);
+
+	QString getMultiLineText(QWidget *parent,
+							 const QString &label,
+							 const QString &title = QString(),
+							 const QString &text = QString(),
+							 bool *ok = Q_NULLPTR,
+							 Qt::InputMethodHints inputMethodHints = Qt::ImhNone);
+
+	QString getText(QWidget *parent,
+					const QString &label,
+					const QString &title,
+					QLineEdit::EchoMode mode = QLineEdit::Normal,
+					const QString &text = QString(),
+					bool *ok = Q_NULLPTR,
+					Qt::InputMethodHints inputMethodHints = Qt::ImhNone);
+
+	// ---------- QProgressDialog ----------
+	QProgressDialog *createProgress(QWidget *parent,
+									const QString &label,
+									const int max = 0,
+									const int min = 0,
+									bool allowCancel = true,
+									const QString &windowTitle = QString(),
+									int minimumDuration = 1000,
+									const QString &cancelButtonText = QString());
+}
+
+#endif // MESSAGEMASTER_H
